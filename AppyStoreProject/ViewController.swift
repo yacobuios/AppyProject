a4adc8963ef35449d61664a460178601670048fd
BridgeLabz <bridgelabz@Narayans-MacBook-Pro-3.local>
2016-07-16 17:17:36 +0530

    updated

--- AppyStoreProject/ViewController.swift
+++ AppyStoreProject/ViewController.swift
@@ -8,14 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UICollectionViewDelegate,UICollectionViewDataSource {
+    @IBOutlet weak var collection: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+    return 100
+    
+    
+}
+
+func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell: UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as! UICollectionViewCell
+    
+    
+    return cell
+}
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
+        
+    {
+        let TableViewController1 = self.storyboard!.instantiateViewControllerWithIdentifier("TableViewController") as! TableViewController
+        self.navigationController?.pushViewController(TableViewController1, animated: true)
+        
+        
+        
+    }
+    
+  override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
